9e5a9e43fd4fcb7bb500805030d458fe426602e3
Aman panwar <amanpanwar123op@gmail.com>
2025-01-29 16:40:59 +0530

    changes in the onTaskAdded

--- Frontend/src/pages/AddTask/AddForm.jsx
+++ Frontend/src/pages/AddTask/AddForm.jsx
@@ -29,7 +29,7 @@ export default function AddForm({ onTaskAdded }) {
                 toast.success("Task added successfully");
                 document.getElementById("my_modal_1").close();
                 onTaskAdded(res.data.taskcreated); // Notify parent component with the new task
-                
+                setTimeout(()=>{window.location.reload();},1000);
                 
             }
         } catch (err) {
